cd00c8e905714a7b95b664355ecf21820ec641a9
ejung <leejungstyle@gmail.com>
2019-12-11 02:32:39 +0900

    test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // index.js
 var express = require('express');
+var router = require('router');
 var pg = require('pg');
+var {Pool} = require('pg');
 var app = express();
 
 // routing
@@ -17,21 +19,26 @@ app.route('/api/folder')
         res.json(result)
     })
     .post(async (req, res) => {
-      var con = "postgres://nrwxoqndcmnssw:50a27b4a4e5419b5da3d0ecd074d62235b0f536c8a33841456a2c3ee14c2ba1d@ec2-23-21-148-223.compute-1.amazonaws.com:5432/d7v4ta77be36ns";
-      pg.connect(con, function (err, client) {
-          var qstr = "insert into sg_data (data) values($data);";
-          var query = client.query(qstr, [req.body]);
-          query.on('end', function (row, err) {
-              response.redirect("/");
-          });
-          query.on('error', function (error) {
-              console.log("ERROR!");
-              response.render('index', {
-                  title: "ERROR",
-                  data: null,
-                  message: "ERROR is occured!"
-              });
-          });
+      //var con = "postgres://nrwxoqndcmnssw:50a27b4a4e5419b5da3d0ecd074d62235b0f536c8a33841456a2c3ee14c2ba1d@ec2-23-21-148-223.compute-1.amazonaws.com:5432/d7v4ta77be36ns";
+      var con = new pg.Client({
+        user: "nrwxoqndcmnssw",
+        password: "50a27b4a4e5419b5da3d0ecd074d62235b0f536c8a33841456a2c3ee14c2ba1d",
+        database: "d7v4ta77be36ns",
+        port: 5432,
+        host: "ec2-23-21-148-223.compute-1.amazonaws.com",
+        ssl: true
+    }); 
+     
+      con.connect(function(err, client, done) {
+
+        if (err) {
+          console.log("not able to get connection " + err);
+          res.status(400).send(err);
+      }
+
+          var qstr = "insert into sg_data (idx, data) values(1,'"+req+"');";
+          var query = client.query(qstr);
+         
       });
       })
 
