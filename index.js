cdc458154f84108f0bd2ff50b18e81f78d11d9f7
ejung <leejungstyle@gmail.com>
2019-12-11 00:22:09 +0900

    test

--- index.js
+++ index.js
@@ -3,16 +3,32 @@
 var express = require('express');
 var app = express();
 
-app.set("view engine","ejs");
-app.use(express.static(__dirname + '/public'));
-
-app.get("/hello", function(req,res){
-  res.render("hello", {name:req.query.nameQuery});
-});
-
-app.get("/hello/:nameParam", function(req,res){
-  res.render("hello", {name:req.params.nameParam});
-});
+// routing
+app.route('/api/folder')
+    .get(async (req, res) => {
+        const result = {success: true}
+        try {
+            const json = await db.getData()
+            result.data = json
+        } catch (err) {
+            result.success = false
+            result.err = err
+        }
+        res.json(result)
+    })
+    .post(async (req, res) => {
+        const result = {success: true}
+        const folder = req.body.folder // 입력 받은 폴더 정보
+        try {
+          const json = await db.getData() // 데이터 읽어오기
+          json.folder = folder  // 데이터 수정
+          await db.setData(json) // 데이터 반영
+        } catch (err) {
+          result.success = false
+          result.err = err
+        }
+        res.json(result) // 결과 출력
+      })
 
 var port = process.env.PORT || 3000; //*
 app.listen(port, function(){
