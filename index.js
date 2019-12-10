911330a447d5713a863ec84a64af1ef05c604978
ejung <leejungstyle@gmail.com>
2019-12-11 05:02:13 +0900

    tet

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // index.js
 var express = require('express');
-var router = require('router');
+//var router = require('router');
 var pg = require('pg');
-var {Pool} = require('pg');
 var app = express();
+//https://fast-bastion-20842.herokuapp.com/api/folder
+//app.use(express.json())  // request body를 사용하기 위함
+app.use(express.json())
+app.use(express.static('public')) // static file을 사용하기 위함
 
 // routing
 app.route('/api/folder')
@@ -19,7 +22,8 @@ app.route('/api/folder')
         res.json(result)
     })
     .post(async (req, res) => {
-      //var con = "postgres://nrwxoqndcmnssw:50a27b4a4e5419b5da3d0ecd074d62235b0f536c8a33841456a2c3ee14c2ba1d@ec2-23-21-148-223.compute-1.amazonaws.com:5432/d7v4ta77be36ns";
+      const result = {success: true}
+      
       var con = new pg.Client({
         user: "nrwxoqndcmnssw",
         password: "50a27b4a4e5419b5da3d0ecd074d62235b0f536c8a33841456a2c3ee14c2ba1d",
@@ -27,21 +31,28 @@ app.route('/api/folder')
         port: 5432,
         host: "ec2-23-21-148-223.compute-1.amazonaws.com",
         ssl: true
-    }); 
-     
+      }); 
+      var text = String(JSON.stringify(req.body));
+      console.log(text);
       con.connect(function(err, client, done) {
-
+        
         if (err) {
           console.log("not able to get connection " + err);
           res.status(400).send(err);
       }
-
-          var qstr = "insert into sg_data (idx, data) values(1,'"+req+"');";
-          var query = client.query(qstr);
+      try{
          
+          var qstr = "insert into sg_data (data) values('"+text+"');";
+          var query = client.query(qstr);
+        }catch(error){
+          result.success = false
+          console.log(error);
+        }
       });
+ 
+      res.json(result)
       })
-
+//ALTER TABLE sg_data ADD COLUMN idx SERIAL PRIMARY KEY; 
 var port = process.env.PORT || 3000; //*
 app.listen(port, function(){
   console.log('Server On!');
